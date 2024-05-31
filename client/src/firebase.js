8a10c1227d4f8ac19eeb5a90f6b2b5505bbd3bc3
Sidd3100 <siddraimb@gmail.com>
2024-05-31 11:30:23 +0530

    server updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+require('dotenv').config();
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATK6Sc83pjbMv-zxUxWtZee7IVyJT59ek",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "mern-sketchsymphony.firebaseapp.com",
   projectId: "mern-sketchsymphony",
-  storageBucket: "mern-sketchsymphony.appspot.com",
+  storageBucket: "mern-sketchsymphony",
   messagingSenderId: "790548907501",
   appId: "1:790548907501:web:61ae238c15345ad81e93e4"
 };
