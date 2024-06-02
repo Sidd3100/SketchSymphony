9fcc1869665fd70901f3b642a17d2780dd8d7f3b
Sidd3100 <103369880+Sidd3100@users.noreply.github.com>
2024-06-02 22:22:59 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server :{
+//     proxy:{
+//       '/api':{
+//         target:'http://localhost:5000',
+//         secure: false,
+//       }
+//     },
+//     port:3000
+//   }
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 export default defineConfig({
   plugins: [react()],
-  server :{
-    proxy:{
-      '/api':{
-        target:'http://localhost:5000',
-        secure: false,
-      }
-    },
-    port:3000
-  }
-})
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+  },
+});
