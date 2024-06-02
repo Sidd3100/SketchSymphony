68ea727a7c210e584e87d74b925280a089f715ef
Sidd3100 <siddraimb@gmail.com>
2024-06-02 22:36:15 +0530

    updated build

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,13 +14,14 @@
 //     port:3000
 //   }
 // })
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: 'dist', // Default output directory
   },
 });
+
