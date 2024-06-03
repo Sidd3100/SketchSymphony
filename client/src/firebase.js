bdd84118ae0f737b2d3efa1ade64bd4db6e88532
Sidd3100 <103369880+Sidd3100@users.noreply.github.com>
2024-06-03 10:29:52 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey:"AIzaSyATK6Sc83pjbMv-zxUxWtZee7IVyJT59ek",
   authDomain: "mern-sketchsymphony.firebaseapp.com",
   projectId: "mern-sketchsymphony",
   storageBucket: "mern-sketchsymphony.appspot.com",
